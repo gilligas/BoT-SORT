2ffce89bc2adb7a8e22d0c598edfa01b40f2cf4d
gilligas <gil-simas@hotmail.com>
2024-07-27 00:01:12 +0100

    fix str error

--- fast_reid/fastreid/data/build.py
+++ fast_reid/fastreid/data/build.py
@@ -8,7 +8,6 @@ import logging
 import os
 
 import torch
-from torch._six import string_classes
 from collections import Mapping
 
 from fast_reid.fastreid.config import configurable
@@ -186,5 +185,5 @@ def fast_batch_collator(batched_inputs):
         return torch.tensor(batched_inputs, dtype=torch.float64)
     elif isinstance(elem, int):
         return torch.tensor(batched_inputs)
-    elif isinstance(elem, string_classes):
+    elif isinstance(elem, str):
         return batched_inputs
