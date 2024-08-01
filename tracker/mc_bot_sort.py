7a3cf465648d9d06da5c4b52d6e6472094bff729
gil-simas@hotmail.com <gil-simas@hotmail.com>
2024-08-01 18:00:19 +0100

    add roi stuff

--- tracker/mc_bot_sort.py
+++ tracker/mc_bot_sort.py
@@ -35,7 +35,11 @@ class STrack(BaseTrack):
             self.update_features(feat)
         self.features = deque([], maxlen=feat_history)
         self.alpha = 0.9
+
+        self.roi = []
         self.path_history = []
+        self.in_roi = False
+        self.last_crossing = 0
 
     def update_features(self, feat):
         feat /= np.linalg.norm(feat)
@@ -148,9 +152,9 @@ class STrack(BaseTrack):
 
 
         new_tlwh = new_track.tlwh
-        print(new_tlwh)
+        bec = self.tlwh_to_bec(new_tlwh)
 
-        self.path_history.append(self.tlwh_to_bec(new_tlwh))
+        self.path_history.append(bec)
 
         self.mean, self.covariance = self.kalman_filter.update(self.mean, self.covariance, self.tlwh_to_xywh(new_tlwh))
 
@@ -162,6 +166,44 @@ class STrack(BaseTrack):
 
         self.score = new_track.score
         self.update_cls(new_track.cls, new_track.score)
+
+        # print("roi", self.roi)
+        # print("bec",bec)
+        # print("in_roi", self.point_in_roi(bec, self.roi))
+
+    @staticmethod
+    def point_in_roi(point, polygon):
+        num_vertices = len(polygon)
+        x, y = point[0], point[1]
+        inside = False
+    
+        # Store the first point in the polygon and initialize the second point
+        p1 = polygon[0]
+    
+        # Loop through each edge in the polygon
+        for i in range(1, num_vertices + 1):
+            # Get the next point in the polygon
+            p2 = polygon[i % num_vertices]
+    
+            # Check if the point is above the minimum y coordinate of the edge
+            if y > min(p1[1], p2[1]):
+                # Check if the point is below the maximum y coordinate of the edge
+                if y <= max(p1[1], p2[1]):
+                    # Check if the point is to the left of the maximum x coordinate of the edge
+                    if x <= max(p1[0], p2[0]):
+                        # Calculate the x-intersection of the line connecting the point to the edge
+                        x_intersection = (y - p1[1]) * (p2[0] - p1[0]) / (p2[1] - p1[1]) + p1[0]
+    
+                        # Check if the point is on the same line as the edge or to the left of the x-intersection
+                        if p1[0] == p2[0] or x <= x_intersection:
+                            # Flip the inside flag
+                            inside = not inside
+    
+            # Store the current point as the first point for the next iteration
+            p1 = p2
+    
+        # Return the value of the inside flag
+        return inside
 
     @staticmethod
     def tlwh_to_bec(tlwh):
@@ -237,7 +279,7 @@ class STrack(BaseTrack):
 
 
 class BoTSORT(object):
-    def __init__(self, args, frame_rate=5):
+    def __init__(self, args, frame_rate=5, roi = []):
 
         self.tracked_stracks = []  # type: list[STrack]
         self.lost_stracks = []  # type: list[STrack]
@@ -263,6 +305,9 @@ class BoTSORT(object):
             self.encoder = FastReIDInterface(args.fast_reid_config, args.fast_reid_weights, args.device)
 
         self.gmc = GMC(method=args.cmc_method, verbose=[args.name, args.ablation])
+
+        #roi
+        self.roi = roi
 
     def update(self, output_results, img):
         self.frame_id += 1
@@ -318,10 +363,12 @@ class BoTSORT(object):
         tracked_stracks = []  # type: list[STrack]
         for track in self.tracked_stracks:
             if not track.is_activated:
+                #create track
                 unconfirmed.append(track)
+                track.roi = self.roi
             else:
                 tracked_stracks.append(track)
-
+            
         ''' Step 2: First association, with high score detection boxes'''
         strack_pool = joint_stracks(tracked_stracks, self.lost_stracks)
 
